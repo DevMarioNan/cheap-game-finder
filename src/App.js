644968e3784468e79306ac6cd49912b644835526
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-05-18 05:29:55 +0300

    ADDED Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import HomePage from './components/HomePage';
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      
     </div>
   );
 }
